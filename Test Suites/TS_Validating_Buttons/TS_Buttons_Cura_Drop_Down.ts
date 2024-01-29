12a6091899c1a5f55596132ef287a705cfa2db2f
shaikmansur12 <mansur_shaik@srmap.edu.in>
2024-01-29 15:21:08 +0530

    Buttons At Test Listener Level - Drop Down

---
+++ Test Suites/TS_Validating_Buttons/TS_Buttons_Cura_Drop_Down.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Buttons_Cura_Drop_Down</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>be4726e6-f3a7-4c3c-802a-e8ebb5d6cbc0</testSuiteGuid>
+   <testCaseLink>
+      <guid>ec8d733c-cdb6-438f-bd7d-ec64c4a50b8d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buttons/TC_CURA_MakeApointmnent_002_Drop_Down_Button_Facility</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>aa3c3841-30e5-4e19-8841-f213f4b4c83d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
