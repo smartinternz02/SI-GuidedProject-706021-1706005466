5a77aef5411085555d7acc906cad4dd837d6d9ff
shaikmansur12 <mansur_shaik@srmap.edu.in>
2024-01-30 15:18:45 +0530

    Cura_Appointment_ To Be Tested in Jenkins

---
+++ Test Suites/TS_Cura_Appointment_Jenkins_CLI_001/TS_Cura_Appointment_Jenkins_001.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Cura_Appointment_Jenkins_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f75c874c-5614-4e75-b228-7eb7d3b46282</testSuiteGuid>
+   <testCaseLink>
+      <guid>8be9dc26-fd15-4e78-a4ce-19f41782f934</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Cura_Make_Appointment_Jenkins_CLI_001/TC_Cura_Appointment_Jenkins_CLI_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
